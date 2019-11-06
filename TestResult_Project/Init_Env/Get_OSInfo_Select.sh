5b92785e405d314ac921425a27c275f299418bee
jianxiamage <jianxiamage@163.com>
2019-11-06 09:42:27 +0800

    删除knownhosts文件，防止远程连接失败

--- TestResult_Project/Init_Env/Get_OSInfo_Select.sh
+++ TestResult_Project/Init_Env/Get_OSInfo_Select.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+#-------------------------------------------
 if [ $# -ne 2 ];then
  echo "usage: $0 TestType Platform" 
  exit 1
 fi
+#-------------------------------------------
 
+#-------------------------------------------
 TestType=$1
 Platform=$2
+#-------------------------------------------
 
+#删除knownhosts文件，防止远程连接出错
+rm -rf /root/.ssh/known_hosts
+
+#-------------------------------------------------------------------------------
 echo "Begin to download result files.TestType:[$TestType],TestPlat:[$Platform]"
 Get_OSInfo="Get_OSInfo_${TestType}_${Platform}"
 sh ${Get_OSInfo}.sh
