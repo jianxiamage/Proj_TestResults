4b32373a1d20427fec5b8a80d9c4ee7f6a174daf
jianxiamage <jianxiamage@163.com>
2019-12-10 14:57:51 +0800

    同步服务器测试结果到本地服务器的功能代码添加log功能

--- TestResult_Project_ver0.3/Rsync_Tool/rsync_testFiles.sh
+++ TestResult_Project_ver0.3/Rsync_Tool/rsync_testFiles.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
 #--------------------------------------------
-ServerIP='auto_test.loongson.cn'
+ServerDomain='auto_test.loongson.cn'
 ServerUser='loongson-test'
 ServerPass='loongson'
 #--------------------------------------------
 
 AutoTestDir='/AutoTest'
-ServerTestDir='autotest_result'
+ServerTestDir='autotest_result1'
 #--------------------------------------------
-
+#域名转IP
+ServerIP=`dig +short $ServerDomain`
+#--------------------------------------------
+LogFile_Rsync=/var/log/local_rsyncd.log
 
 echo "***************************************************"
 start_time=`date +%s`              #定义脚本运行的开始时间
 
+sshpass -p $ServerPass  ssh -o StrictHostKeychecking=no $ServerUser@$ServerDomain \
+"[ -d ~/${ServerTestDir} ]" 
+retCode=$?
 
-sshpass -p $ServerPass rsync -avz --delete -e 'ssh -p 22' \
-$ServerUser@$ServerIP:~/$ServerTestDir $AutoTestDir
+if [ $retCode -ne 0 ];
+then 
+  echo "Error,No Directory named [${ServerTestDir}] on Server:[${ServerIP}]!"
+  exit 1
+fi
 
-#rsync -avz --delete $ServerUser@$ServerIP:~/$ServerTestDir $AutoTestDir
+rm -rf ${LogFile_Rsync}
+sshpass -p $ServerPass rsync -avzP --delete -e 'ssh -p 22' --log-file="${LogFile_Rsync}" \
+$ServerUser@$ServerDomain:~/$ServerTestDir $AutoTestDir
+retCode=$?
+
+if [ $retCode -ne 0 ];
+then
+  echo "Error,Rsync data with Server:[${ServerIP}] failed!"
+  exit 1
+fi
+
+#rsync -avz --delete $ServerUser@$ServerDomain:~/$ServerTestDir $AutoTestDir
 
 stop_time=`date +%s`  #定义脚本运行的结束时间
+
 echo "***************************************************"
 echo "Exec Time:`expr $stop_time - $start_time`s"
 echo "***************************************************"
