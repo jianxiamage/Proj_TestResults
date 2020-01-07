bb7adcb344fcdc9fee076a4cab720bb64fe1f0be
jianxiamage <jianxiamage@163.com>
2020-01-07 14:26:54 +0800

    修改同步远程ftp文件的参数，避免同步失败的情况

--- TestResult_Project_ver0.3/Init_Env/getIPList_fromServer.sh
+++ TestResult_Project_ver0.3/Init_Env/getIPList_fromServer.sh
@@ -9,5 +9,11 @@ dir_Server=$(cat Dir_Server.txt)
 dir_Local=$(cat Dir_Local.txt)
 #--------------------------------------------------------------------------------------------------
 
+#示例:
 #lftp -e "mirror --delete --only-newer --verbose /tmp/AutoTest/IP_List IP_List;exit" ${IP_Server}
-lftp -e "mirror --delete --only-newer --verbose ${dir_Server} ${dir_Local};exit" ${IP_Server}
+#lftp -e "mirror --delete --only-newer --verbose ${dir_Server} ${dir_Local};exit" ${IP_Server}
+#实际执行时,不需要参数--only-newer,因为远程机器上的文件可能没有本地新，但还是需要下载远程同名文件
+
+#实际执行
+lftp -e "mirror --delete --verbose ${dir_Server} ${dir_Local};exit" ${IP_Server}
+
